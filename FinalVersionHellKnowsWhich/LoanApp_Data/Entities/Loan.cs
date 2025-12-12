01c92ebbfc25efd260bc204326aa405ccdbb88f0
Nin4x <ninchexxdarcho@gmail.com>
2025-12-12 22:52:01 +0400

    adding User and Loan classes

---
+++ FinalVersionHellKnowsWhich/LoanApp_Data/Entities/Loan.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalVersionHellKnowsWhich.LoanApp_Data.Entities
+{
+    public class Loan
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public LoanType Type { get; set; }
+        [Required]
+        public decimal Amount { get; set; } 
+        [Required]
+        public Currency Currency { get; set; }
+        [Required]
+        public int PeriodMonths { get; set; } 
+        public LoanStatus Status { get; set; } = LoanStatus.Pending;
+        [Required]
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+
+    public enum LoanStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+
+    public enum LoanType
+    {
+        QuickLoan,
+        Auto,
+        Installment
+    }
+
+    public enum Currency
+    {
+        USD,
+        EUR,
+        GEL
+    }
+}
---
+++ FinalVersionHellKnowsWhich/LoanApp_Data/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+
+namespace FinalVersionHellKnowsWhich.LoanApp_Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } 
+        public int Age { get; set; }
+        public decimal MonthlyIncome { get; set; }
+        public bool IsBlocked { get; set; } = false;
+        public Role UserRole { get; set; } = Role.User;
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+
+    }
+
+    public enum Role { User, Accountant }
+}
