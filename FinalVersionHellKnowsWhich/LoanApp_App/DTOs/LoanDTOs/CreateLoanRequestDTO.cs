1b0902c58b13874b46e6b67d36c5ca2f774267ec
Nin4x <ninchexxdarcho@gmail.com>
2025-12-14 20:29:18 +0400

    hope this works -  added some loan controllers and user block unblock staff

--- FinalVersionHellKnowsWhich/LoanApp_App/DTOs/LoanDTOs/CreateLoanRequestDTO.cs
+++ FinalVersionHellKnowsWhich/LoanApp_App/DTOs/LoanDTOs/CreateLoanRequestDTO.cs
@@ -2,11 +2,16 @@
 using FinalVersionHellKnowsWhich.LoanApp_Data.Enums;
 namespace FinalVersionHellKnowsWhich.LoanApp_App.DTOs.LoanDTOs
 {
-    public class CreateLoanRequestDTO
+    public class UserCreateLoanRequestDTO
     {
         public required LoanType Type { get; set; }
         public Currency Currency { get; set; }
         public decimal Amount { get; set; }
         public int PeriodMonths { get; set; }
+    }
+
+    public class AccountantCreateLoanRequestDTO: UserCreateLoanRequestDTO
+    {
+        public required Guid UserId { get; set; }
     }
 }
